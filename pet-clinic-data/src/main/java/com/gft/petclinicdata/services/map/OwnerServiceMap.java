05f15528ead9509fc1c06e72fbef01138d5180e8
Alejandro Lopez <alopez.externe@macif.fr>
2019-12-02 13:35:44 -0500

    added ServiceMap

---
+++ pet-clinic-data/src/main/java/com/gft/petclinicdata/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package com.gft.petclinicdata.services.map;
+
+import com.gft.petclinicdata.model.Owner;
+import com.gft.petclinicdata.services.CrudService;
+import com.gft.petclinicdata.services.OwnerService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
