ecbc45fe16936e4a8a92eb6f395320e6727c53c9
Alejandro Lopez <alopez.externe@macif.fr>
2019-12-03 10:26:33 -0500

    refactor save object to automatically set the id

--- pet-clinic-web/src/main/java/com/gft/petclinicweb/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/gft/petclinicweb/bootstrap/DataLoader.java
@@ -25,12 +25,10 @@ public class DataLoader implements CommandLineRunner {
         Owner owner = new Owner();
         owner.setFirstName("toto");
         owner.setLastName("lopez");
-        owner.setId(1L);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("titi");
         owner2.setLastName("Titi");
-        owner2.setId(2L);
 
         ownerService.save(owner);
         ownerService.save(owner2);
@@ -38,12 +36,10 @@ public class DataLoader implements CommandLineRunner {
         Vet vet = new Vet();
         vet.setFirstName("Foo");
         vet.setLastName("bar");
-        vet.setId(1L);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Vet");
         vet2.setLastName("Vet");
-        vet2.setId(2L);
 
         vetService.save(vet);
         vetService.save(vet2);
